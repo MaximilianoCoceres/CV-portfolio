6ae838f922594c7f7cdfc633768db4e0bf1f8580
MaximilianoCoceres <maximilianococeres7@gmail.com>
2022-06-24 20:10:38 +0200

    correcciones sebi

--- efectos.js
+++ efectos.js
@@ -1,6 +1,6 @@
-const item      = document.querySelectorAll('.item')   
+const item = document.querySelectorAll('.item')
 const principal = document.querySelectorAll('.principal')
-const titulo     = document.querySelectorAll('.titulo')   
+const titulo = document.querySelectorAll('.titulo')
 const parrafo = document.querySelectorAll('.parrafo-about')
 const sliderContainer = document.getElementById('slider-container');
 const slider = document.getElementById('slider');
@@ -12,22 +12,22 @@ const sliderElements = document.querySelectorAll('.slider__element');
 let sliderCounter = 0;
 let isInTransition = false;
 
-const DIRECTION ={
-    RIGHT:'RIGHT',
-    LEFT:'LEFT'
+const DIRECTION = {
+    RIGHT: 'RIGHT',
+    LEFT: 'LEFT'
 };
 
 
-item.forEach((cadaItem, i) =>{
-    item[i].addEventListener('click',()=>{
-        item.forEach((cadaItem,j)=>{
-        
+item.forEach((cadaItem, i) => {
+    item[i].addEventListener('click', () => {
+        item.forEach((cadaItem, j) => {
+
             item[j].classList.remove('activo');
             principal[j].classList.remove('activo');
             titulo[i].classList.remove('activo');
-        parrafo[i].classList.remove('activo');
+            parrafo[i].classList.remove('activo');
         })
-        
+
         item[i].classList.add('activo');
         principal[i].classList.add('activo');
     });
@@ -36,14 +36,14 @@ item.forEach((cadaItem, i) =>{
 
 
 
-titulo.forEach((cadaTitulo, i) =>{
-    titulo[i].addEventListener('click',()=>{
+titulo.forEach((cadaTitulo, i) => {
+    titulo[i].addEventListener('click', () => {
         // item.forEach((cadaTitulo,j)=>{
-        
+
         //     // titulo[j].classList.remove('activo');
         //     // parrafo[j].classList.remove('activo');
         // })
-        
+
         titulo[i].classList.toggle('activo');
         parrafo[i].classList.toggle('activo');
     });
@@ -51,17 +51,17 @@ titulo.forEach((cadaTitulo, i) =>{
 
 });
 
-const getTransformValue = ()=>
-Number(rootStyle.getPropertyValue('--slider-transform').replace('px',''));
+const getTransformValue = () =>
+    Number(rootStyle.getPropertyValue('--slider-transform').replace('px', ''));
 
-const reorderSlide = ()=>{
+const reorderSlide = () => {
     const transformValue = getTransformValue();
-    rootStyle.setProperty('--transition','none');
-    if(sliderCounter===sliderElements.length -1){   
+    rootStyle.setProperty('--transition', 'none');
+    if (sliderCounter === sliderElements.length - 1) {
         slider.appendChild(slider.firstElementChild);
         rootStyle.setProperty('--slider-transform', `${transformValue + sliderElements[sliderCounter].scrollWidth}px`);
         sliderCounter--;
-    }else if(sliderCounter===0) {
+    } else if (sliderCounter === 0) {
         slider.prepend(slider.lastElementChild);
         rootStyle.setProperty('--slider-transform', `${transformValue - sliderElements[sliderCounter].scrollWidth}px`);
         sliderCounter++;
@@ -70,24 +70,26 @@ const reorderSlide = ()=>{
     isInTransition = false;
 }
 
-const moveSlide=(direction)=>{
-    if(isInTransition)return;
+const moveSlide = (direction) => {
+    if (isInTransition) return;
     const transformValue = getTransformValue();
-    rootStyle.setProperty('--transition','transform 1s');
+    rootStyle.setProperty('--transition', 'transform 1s');
     isInTransition = true;
-    if (direction===DIRECTION.LEFT){
+    if (direction === DIRECTION.LEFT) {
         rootStyle.setProperty('--slider-transform', `${transformValue + sliderElements[sliderCounter].scrollWidth}px`);
         sliderCounter--;
-    }else if(direction===DIRECTION.RIGHT){
+    } else if (direction === DIRECTION.RIGHT) {
         rootStyle.setProperty('--slider-transform', `${transformValue - sliderElements[sliderCounter].scrollWidth}px`);
         sliderCounter++;
     }
 }
 
 
-buttonRight.addEventListener('click',()=>moveSlide(DIRECTION.RIGHT));
-buttonLeft.addEventListener('click',()=>moveSlide(DIRECTION.LEFT));
+buttonRight.addEventListener('click', () => moveSlide(DIRECTION.RIGHT));
+buttonLeft.addEventListener('click', () => moveSlide(DIRECTION.LEFT));
 
 slider.addEventListener('transitionend', reorderSlide);
 
 reorderSlide();
+
+window.addEventListener('resize', reorderSlide)
