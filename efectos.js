3f377b9053570ba173f65e9a60bda211e662429b
MaximilianoCoceres <maximilianococeres7@gmail.com>
2022-07-13 22:03:54 +0200

    cambnio imagen total del cv

--- efectos.js
+++ efectos.js
@@ -1,7 +1,7 @@
 const item = document.querySelectorAll('.item')
-const principal = document.querySelectorAll('.principal')
-const titulo = document.querySelectorAll('.titulo')
-const parrafo = document.querySelectorAll('.parrafo-about')
+const principal = document.querySelectorAll('.sobre-mi')
+
+
 const sliderContainer = document.getElementById('slider-container');
 const slider = document.getElementById('slider');
 const buttonLeft = document.getElementById('button-left');
@@ -20,38 +20,21 @@ const DIRECTION = {
 };
 
 
-item.forEach((cadaItem, i) => {
-    item[i].addEventListener('click', () => {
-        item.forEach((cadaItem, j) => {
+// item.forEach((cadaItem, i) => {
+//     item[i].addEventListener('click', () => {
+//         item.forEach((cadaItem, j) => {
 
-            item[j].classList.remove('activo');
-            principal[j].classList.remove('activo');
-            titulo[i].classList.remove('activo');
-            parrafo[i].classList.remove('activo');
-        })
-
-        item[i].classList.add('activo');
-        principal[i].classList.add('activo');
-    });
-});
+//             item[j].classList.remove('activo');
+//             principal[j].classList.remove('activo');
 
 
+//         })
 
+//         item[i].classList.add('activo');
+//         principal[i].classList.add('activo');
+//     });
+// });
 
-titulo.forEach((cadaTitulo, i) => {
-    titulo[i].addEventListener('click', () => {
-        // item.forEach((cadaTitulo,j)=>{
-
-        //     // titulo[j].classList.remove('activo');
-        //     // parrafo[j].classList.remove('activo');
-        // })
-
-        titulo[i].classList.toggle('activo');
-        parrafo[i].classList.toggle('activo');
-    });
-
-
-});
 
 const getTransformValue = () =>
     Number(rootStyle.getPropertyValue('--slider-transform').replace('px', ''));
@@ -99,18 +82,18 @@ window.addEventListener('resize', reorderSlide)
 
 $form.addEventListener('submit', handleSubmit)
 
-function handleSubmit(event){
+function handleSubmit(event) {
     event.preventDefault()
     const form = new FormData(this)
-    $button.setAttribute('href',`mailto:maxiprog7@gmail.com?subject=${form.get('name')} ${form.get('mail')} &body=${form.get('comentario')}`)
+    $button.setAttribute('href', `mailto:maxiprog7@gmail.com?subject=${form.get('name')} ${form.get('mail')} &body=${form.get('comentario')}`)
     $button.click()
 }
 
 
-window.addEventListener("scroll",function(){
+window.addEventListener("scroll", function () {
     var header = this.document.querySelector('header');
-    var footer = this.document.querySelector('footer')
-    header.classList.toggle("abajo",window.scrollY>0);
-    footer.classList.toggle("abajo",window.scrollY>0)
+    var footer = this.document.querySelector('footer');
+    header.classList.toggle("abajo", window.scrollY > 0);
+    footer.classList.toggle("abajo", window.scrollY > 0);
 
 })
